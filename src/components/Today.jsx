50b4a013ff43f35193815b733856cf3912356494
Jon <jonsairmail@icloud.com>
2024-08-05 15:02:39 -0700

    main functionality completed

--- src/components/Today.jsx
+++ src/components/Today.jsx
@@ -2,7 +2,7 @@ import {useState} from 'react'
 
 function today() {
   // Active reminders:
-  const [task, setTask] = useState([]); // (stored in an array)
+  const [task, setTask] = useState(['eat breakfast', 'shower', 'walk the dog']); // (stored in an array)
 
   // Create new reminder:
   const [newTask, setNewTask] = useState('');
@@ -13,16 +13,32 @@ function today() {
     setNewTask(event.target.value)
   }
   function addTask(){
-
+    if(newTask.trim() !== ''){
+      setTask(t => [...t, newTask])
+      setNewTask('')
+    }
   }
   function deleteTask(index){
-
+    const updatedTasks = task.filter((_, i) => i !== index)
+    setTask(updatedTasks)
   }
   function raise(index){
-
+    if(index > 0){
+      const updatedTasks = [...task];
+      // swap two elements within an array
+      [updatedTasks[index], updatedTasks[index -1]] = 
+      [updatedTasks[index - 1], updatedTasks[index]];
+      setTask(updatedTasks);
+    }
   }
   function lower(index){
-    
+    if(index < task.length - 1){
+      const updatedTasks = [...task];
+      // swap two elements within an array
+      [updatedTasks[index], updatedTasks[index + 1]] =
+      [updatedTasks[index + 1], updatedTasks[index]];
+      setTask(updatedTasks);
+    }
   }
 
   return (
@@ -31,7 +47,19 @@ function today() {
       <h1>Today</h1>
       <div>
         <input type='text' placeholder='Buy milk' value={newTask} onChange={handleInputChange}/>
+        <button onClick={addTask}>+</button>
       </div>
+
+      <ul className='list-items'>
+        {task.map((task, index) =>
+        <li key={index}>
+          <span className='list-text'>{task}</span>
+          <button className='delete-button' onClick={() => deleteTask(index)}>delete</button>
+          <button className='raise-button' onClick={() => raise(index)}>raise</button>
+          <button className='lower-button' onClick={() => lower(index)}>lower</button>
+        </li>
+        )}
+      </ul>
     </div>
     </>
   )
