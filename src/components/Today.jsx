90655ef3869589a4c0fba0f7b2306df9b0c7ea4c
Jon <jonsairmail@icloud.com>
2024-08-04 17:53:52 -0700

    added functions

--- src/components/Today.jsx
+++ src/components/Today.jsx
@@ -1,15 +1,38 @@
 import {useState} from 'react'
 
 function today() {
-  // active reminders (stored in an array)
-  const [task, setTask] = useState([]);
-  
-  // Create new reminder
+  // Active reminders:
+  const [task, setTask] = useState([]); // (stored in an array)
+
+  // Create new reminder:
   const [newTask, setNewTask] = useState('');
+  
+
+  // Textbox to type something:
+  function handleInputChange(event){
+    setNewTask(event.target.value)
+  }
+  function addTask(){
+
+  }
+  function deleteTask(index){
+
+  }
+  function raise(index){
+
+  }
+  function lower(index){
+    
+  }
 
   return (
     <>
-      
+    <div className='to-do-list'>
+      <h1>Today</h1>
+      <div>
+        <input type='text' placeholder='Buy milk' value={newTask} onChange={handleInputChange}/>
+      </div>
+    </div>
     </>
   )
 }
